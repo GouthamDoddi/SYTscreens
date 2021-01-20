b809b3f4c81a471e35a6727d72a2488eb19fea17
goutham Doddi <gouthambolt@gmail.com>
2021-01-20 08:02:20 +0530

    initial commit

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App () {
+  let x = 1;
+
+  console.log('excuted');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi there this is goutham!</Text>
       <StatusBar style="auto" />
     </View>
   );
